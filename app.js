cd18a1b0585a4d515b674d75d19e5075c1990d4c
unknown <ganesh.srambikal@gmail.com>
2018-10-21 19:51:38 +0530

    added routes OK.

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const users = require('./routes/users');
+
+
 const port = 3000;
+//cors middleware
+app.use(cors());
+
+//body parser middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
 
 app.get('/', (req, res) => {
     res.send('Invalid connect');
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+//register
+router.get('/register', (req, res, next) => {
+    res.send('REGISTER');
+});
+
+//authentication
+router.get('/authenticate', (req, res, next) => {
+    res.send('authenticate');
+});
+//profile
+router.get('/profile', (req, res, next) => {
+    res.send('profile');
+});
+
+router.get('/validate', (req, res, next) => {
+    res.send('validate');
+});
+
+module.exports = router;
