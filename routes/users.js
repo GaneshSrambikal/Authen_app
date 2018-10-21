95d7287e7a3d659d4109048d5643a68dea10c0f5
unknown <ganesh.srambikal@gmail.com>
2018-10-21 20:55:43 +0530

    added models and register to db OK

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 //register
-router.get('/register', (req, res, next) => {
-    res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({ success: false, msg: 'failed to register user' });
+        }
+        else {
+            res.json({ success: true, msg: 'User registered' });
+        }
+    })
 });
 
 //authentication
@@ -15,8 +32,6 @@ router.get('/profile', (req, res, next) => {
     res.send('profile');
 });
 
-router.get('/validate', (req, res, next) => {
-    res.send('validate');
-});
+
 
 module.exports = router;
