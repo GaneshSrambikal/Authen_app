7ef5d5445d9814b7d33ff2d0c92352fbfed6ce0c
unknown <ganesh.srambikal@gmail.com>
2018-10-21 20:04:55 +0530

    added config , database(authen) OK

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//connect to db
+mongoose.connect(config.database);
+
+//on connected
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongodb' + config.database);
+
+});
+
+//on db connection error
+mongoose.connection.on('error', (err) => {
+    console.log('Connected to mongodb' + err);
+
+});
 
 const app = express();
 
@@ -13,6 +29,9 @@ const users = require('./routes/users');
 const port = 3000;
 //cors middleware
 app.use(cors());
+
+//seet statc folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //body parser middleware
 app.use(bodyParser.json());
